2d043303ae8b6c70662d3b2ebe2a4a77e9b4df93
André Davis <CubicleJockey@users.noreply.github.com>
2023-07-01 11:18:15 -0700

    Update hopsitaloverallranking-per-state-and-rank.sql

--- Classes/D211 - Advanced Data Acquisition/Data/Postgres/OnDemandLab.Scripts/hopsitaloverallranking-per-state-and-rank.sql
+++ Classes/D211 - Advanced Data Acquisition/Data/Postgres/OnDemandLab.Scripts/hopsitaloverallranking-per-state-and-rank.sql
@@ -1,3 +1,30 @@
+/*
+ File: hospitaloverallranking-per-state-and-rank.sql
+ Student: André Davis
+ Student ID: 010630641
+ Performance Assesment: SLM1 — TASK 1: DATA ANALYSIS - Advanced Acquisition - D211
+ Description:
+ 	Each state has an Hospital Overall Ranking which is on a scale of 1-5 or "Not Available".
+	
+	This query groups the Hospital OVer All Rankings into their state buckets. From there with Paritioning
+	the Rankings are applied to the Hospital Overall Rankings to Rank the scaling.
+
+	Example:
+	
+	Alaska Hospital Overall Rating Scores:
+	
+	Scale:				Rank For State:
+	------				---------------
+	1					1
+	2					5
+	3					6
+	4					4
+	5					2
+	"Not Available"		3
+	
+	In this example a score of 1 took the Rank of 1 meaning that their top score is a Hospital Overall Raking of 1.
+	This means they worst score is their most selected rating and that should be looked into.
+*/
 WITH State_OverallRanking_By_State_And_Rating AS (
 	SELECT rating."State" AS State
 		  ,rating."HospitalOverallRating" AS HospitalOverallRating
